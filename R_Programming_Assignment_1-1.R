0e91703cb629fb8d9cfcc1b4d7548fbde61b4921
“sangwoo” <victory9536@gmail.com>
2023-07-25 20:26:38 +0900

    Initial commit

---
+++ R_Programming_Assignment_1-1.R
@@ -0,0 +1,33 @@
+pollutantmean <- function(directory, pollutant, id = 1:332){
+  sums <- vector("numeric", length = length(id))
+  lengths <- vector("numeric", length = length(id))
+  for(i in id){
+    if(i >=1 && i <= 9){
+      data <- read.csv(paste('00',as.character(i),'.csv', sep = ''))
+      goodpollutant <- complete.cases(data[, pollutant])
+      completedata <- data[goodpollutant, pollutant]
+      sums[i - id[1] + 1] <- sum(completedata)
+      lengths[i - id[1] + 1] <- length(completedata)
+    }
+    else if(i >=10 && i <= 99){
+      data <- read.csv(paste('0',as.character(i),'.csv', sep = ''))
+      goodpollutant <- complete.cases(data[, pollutant])
+      completedata <- data[goodpollutant, pollutant]
+      sums[i - id[1] + 1] <- sum(completedata)
+      lengths[i - id[1] + 1] <- length(completedata)
+    }
+    else {
+      data <- read.csv(paste(as.character(i),'.csv', sep = ''))
+      goodpollutant <- complete.cases(data[, pollutant])
+      completedata <- data[goodpollutant, pollutant]
+      sums[i - id[1] + 1] <- sum(completedata)
+      lengths[i - id[1] + 1] <- length(completedata)
+    }
+  }
+
+  finalmean <- sum(sums)/sum(lengths)
+  finalmean
+
+}
+
+
